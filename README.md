d43db6a7af8f58994ba66f6219239e15ffd852f2
Karel Kubat <karel@kubat.nl>
2023-11-21 22:07:03 +0100

    setup

---
+++ README.md
@@ -0,0 +1,5 @@
+# puddle: A simplified worker pool for Go
+
+ <!-- toc -->
+ <!-- /toc -->
+ 
---
+++ puddle_test.go
@@ -0,0 +1,9 @@
+package puddle
+
+import (
+	"testing"
+)
+
+func TestAll(t *testing.T) {
+	// TODO: Add tests
+}
